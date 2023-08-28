a1e20c108ffe6d40957c4c560e57f8424210cbaf
JCCo-Pilot <111530455+JCCo-Pilot@users.noreply.github.com>
2023-08-28 06:58:04 -0500

    buswork

--- src/treasurePlacement.java
+++ src/treasurePlacement.java
@@ -4,7 +4,30 @@ public class treasurePlacement {
     private ArrayList<Integer> xValues;
     private ArrayList<Integer> yValues;
     public treasurePlacement(){
-        xValues.add(660);
+        int x = 660;
+        int y = 50;
+        xValues = new ArrayList<Integer>();
+        yValues = new ArrayList<Integer>();
+        for (int i = 0; i<6;i++){
+            xValues.add(x);
+            yValues.add(y);
+            x+=160;
+        }
+        x = 110;
+        y = 230;
+        for (int i = 0; i<11;i++){
+            xValues.add(x);
+            yValues.add(y);
+            x+=160;
+        }
+        x = 110;
+        y +=180;
+        for (int i = 0; i<11;i++){
+            xValues.add(x);
+            yValues.add(y);
+            x+=160;
+        }
+        /*xValues.add(660);
         yValues.add(50);
         xValues.add(820);
         yValues.add(50);
@@ -15,6 +38,12 @@ public class treasurePlacement {
         xValues.add();
         yValues.add();
         xValues.add();
-        yValues.add();
+        yValues.add();*/
+    }
+    public int getX(int index){
+        return xValues.get(index);
+    }
+    public int getY(int index){
+        return yValues.get(index);
     }
 }
